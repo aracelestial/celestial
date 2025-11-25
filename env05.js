81af20a3cac43342a219c2f1c5e54d5bab699e84
aracelestial <araceli.nulee@gmail.com>
2025-11-24 21:17:08 -0800

    Update env05.js

--- env05.js
+++ env05.js
@@ -1,3 +1,21 @@
+// declaring an array with the name Planets
+let Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn" , "Uranus", "Neptune"];
+
+// declaring an object with name ancientLife
+let ancientLife = {
+    type: "extremophiles",
+    bioma: ["volcanic", "aquatic", "silicon",],
+};
+
+let megaSentence;
+
+megaSentence = "<p3>The celestial bodies that I am interested in are those that may contain traces of life: " + Planets[3] + ", " + Planets[5] + ", "+ Planets[6] + ". </p3>";
+
+megaSentence = megaSentence + "<p4>The most intriguing ones are: " + ancientLife.type + " that are " + ancientLife.bioma[1] + "</p4>";
+
+$("#output").html(megaSentence);
+
+
 var count = 0;
 let colorCount = 0;
 var colors = ["black", "red", "white"];
@@ -39,19 +57,3 @@ $("#eye-button").click(function () {
    if (colorCount == 3) { colorCount = 0; }
 });
 
-// declaring an array with the name Planets
-let Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn" , "Uranus", "Neptune"];
-
-// declaring an object with name ancientLife
-let ancientLife = {
-    type: "extremophiles",
-    bioma: ["volcanic", "aquatic", "silicon",],
-};
-
-let megaSentence;
-
-megaSentence = "<p3>The celestial bodies that I am interested in are those that may contain traces of life: " + Planets[3] + ", " + Planets[5] + ", "+ Planets[6] + ". </p3>";
-
-megaSentence = megaSentence + "<p4>The most intriguing ones are: " + ancientLife.type + " that are " + ancientLife.bioma[1] + "</p4>";
-
-$("#output").html(megaSentence);
